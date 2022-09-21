299487cc2f6ef19d7dac12dbee15b5c3e8efcea0
viljo <anders@viljo.se>
2022-09-21 17:06:05 +0200

    Comments and abort counter

--- idneo/git_clone_svn.py
+++ idneo/git_clone_svn.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+# Clones SVN repos using GIT SVN.
+# 1. Check if the repos are accessable
+# 2. Clone accessable repos, retry after 2,5s delay if clone fails (this is due to frequent connection issues)
+
 import sys, string, os, time, subprocess
 
 server = "https://svn.idneo.com/svn/"
@@ -29,11 +33,13 @@ print("These repos can be reached: ")
 print(tested_repos)
 
 
+error_counter = 255
 
 for i in range(len(tested_repos)):
     path =  server + tested_repos[i]
     command = "git svn clone -s -r `svn info " + path + " | grep Revision | cut -d' ' -f2` " + path + " -T trunk -b branches -t tags"
     print(command)
-    while (os.system(command)):
+    while (os.system(command)) or error_counter:
+        error_counter = error_counter - 1
         print("retrying.." + tested_repos[i])
         time.sleep(2.5)	
